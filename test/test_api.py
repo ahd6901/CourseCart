53507ed8a9b634767ba40b3be8170b3f0a1bdd1a
Amy Do <ahd6901@rit.edu>
2020-12-03 08:46:35 -0500

    setup

---
+++ test/test_api.py
@@ -0,0 +1,13 @@
+import unittest
+# import json
+# from rest_utils import *
+
+from test.rest_utils import get_rest_call
+
+
+class TestExample(unittest.TestCase):
+    def test_api(self):
+        result = get_rest_call(self, 'http://localhost:5000/example_api')
+        self.assertEqual(9, result[0],"Should have returned a count of '9'")
+        print("API test successfully returned a count of '9' ")
+
